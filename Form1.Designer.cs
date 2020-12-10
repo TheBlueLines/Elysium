5db40cbbb9923a094066ab19d6d8823516bbd8d3
TheBlueLines <davidfeher42@gmail.com>
2020-12-10 13:32:38 +0100

    Update 0.1 BETA 1
    This is the first upload!

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace Elysium_MC_Server_Creator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.path = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Start = new System.Windows.Forms.Button();
+            this.name = new System.Windows.Forms.TextBox();
+            this.go = new System.Windows.Forms.Button();
+            this.properties = new System.Windows.Forms.Button();
+            this.plugins = new System.Windows.Forms.Button();
+            this.RunArgument = new System.Windows.Forms.Button();
+            this.Scripts = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // path
+            // 
+            this.path.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.path.Location = new System.Drawing.Point(218, 12);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(300, 26);
+            this.path.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Start
+            // 
+            this.Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Start.Location = new System.Drawing.Point(524, 12);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 26);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // name
+            // 
+            this.name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.name.Location = new System.Drawing.Point(12, 12);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(200, 26);
+            this.name.TabIndex = 2;
+            this.name.TextChanged += new System.EventHandler(this.name_TextChanged);
+            // 
+            // go
+            // 
+            this.go.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.go.Location = new System.Drawing.Point(12, 120);
+            this.go.Name = "go";
+            this.go.Size = new System.Drawing.Size(587, 44);
+            this.go.TabIndex = 3;
+            this.go.Text = "Start Minecraft Server";
+            this.go.UseVisualStyleBackColor = true;
+            this.go.Click += new System.EventHandler(this.go_Click);
+            // 
+            // properties
+            // 
+            this.properties.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.properties.Location = new System.Drawing.Point(12, 44);
+            this.properties.Name = "properties";
+            this.properties.Size = new System.Drawing.Size(290, 32);
+            this.properties.TabIndex = 4;
+            this.properties.Text = "Server Properties";
+            this.properties.UseVisualStyleBackColor = true;
+            this.properties.Click += new System.EventHandler(this.properties_Click);
+            // 
+            // plugins
+            // 
+            this.plugins.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.plugins.Location = new System.Drawing.Point(309, 44);
+            this.plugins.Name = "plugins";
+            this.plugins.Size = new System.Drawing.Size(290, 32);
+            this.plugins.TabIndex = 5;
+            this.plugins.Text = "Plugins";
+            this.plugins.UseVisualStyleBackColor = true;
+            this.plugins.Click += new System.EventHandler(this.plugins_Click);
+            // 
+            // RunArgument
+            // 
+            this.RunArgument.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.RunArgument.Location = new System.Drawing.Point(12, 82);
+            this.RunArgument.Name = "RunArgument";
+            this.RunArgument.Size = new System.Drawing.Size(290, 32);
+            this.RunArgument.TabIndex = 6;
+            this.RunArgument.Text = "Server Run Arguments";
+            this.RunArgument.UseVisualStyleBackColor = true;
+            this.RunArgument.Click += new System.EventHandler(this.RunArgument_Click);
+            // 
+            // Scripts
+            // 
+            this.Scripts.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Scripts.Location = new System.Drawing.Point(309, 82);
+            this.Scripts.Name = "Scripts";
+            this.Scripts.Size = new System.Drawing.Size(290, 32);
+            this.Scripts.TabIndex = 7;
+            this.Scripts.Text = "Scripts";
+            this.Scripts.UseVisualStyleBackColor = true;
+            this.Scripts.Click += new System.EventHandler(this.Scripts_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 175);
+            this.Controls.Add(this.Scripts);
+            this.Controls.Add(this.RunArgument);
+            this.Controls.Add(this.plugins);
+            this.Controls.Add(this.properties);
+            this.Controls.Add(this.go);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.path);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Elysium: Minecraft Server Creator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox path;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.Button go;
+        private System.Windows.Forms.Button properties;
+        private System.Windows.Forms.Button plugins;
+        private System.Windows.Forms.Button RunArgument;
+        private System.Windows.Forms.Button Scripts;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace Elysium_MC_Server_Creator
+{
+    public partial class Form1 : Form
+    {
+        public string work = @"C:\Elysium";
+        public string run;
+        public Form1()
+        {
+            InitializeComponent();
+            path.Text = work+@"\Servers\elysium";
+        }
+        private void name_TextChanged(object sender, EventArgs e)
+        {
+            if (name.Text == "") {
+                path.Text = work + @"\Servers\elysium";
+            }
+            else
+            {
+                path.Text = work + @"\Servers\" + name.Text;
+            }
+        }
+        private void Start_Click(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory(work);
+            Directory.CreateDirectory(work + @"\Servers");
+            Directory.CreateDirectory(path.Text);
+            try
+            {
+                run = File.ReadAllText(path.Text + @"\run.bat");
+                Console.WriteLine("Server found!");
+            }
+            catch
+            {
+                Console.WriteLine("Server not found!");
+                Console.WriteLine("Downloading server...");
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile("http://darknight.hu:8080/server.jar", path.Text+@"\server.jar");
+                Console.WriteLine("Downloaded!");
+                File.WriteAllText(path.Text + @"\run.bat", "@echo off\ncd "+path.Text+"\njava -Xmx2G -jar server.jar\nPAUSE");
+                File.WriteAllText(path.Text + @"\eula.txt","eula=false");
+            }
+        }
+        private void go_Click(object sender, EventArgs e)
+        {
+            if (File.ReadAllText(path.Text + @"\eula.txt") != "eula=true")
+            {
+                ProcessStartInfo eula = new ProcessStartInfo("https://account.mojang.com/documents/minecraft_eula");
+                Process.Start(eula);
+                DialogResult result = MessageBox.Show("By pressing " + DialogResult.Yes + " below you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula)", "You need to agree to the EULA in order to run the server", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    File.WriteAllText(path.Text + @"\eula.txt", "eula=true");
+                }
+                else if (result == DialogResult.No)
+                {
+                    File.WriteAllText(path.Text + @"\eula.txt", "eula=false");
+                }
+            }
+            ProcessStartInfo server = new ProcessStartInfo
+            {
+                FileName = path.Text + @"\run.bat"
+            };
+            Console.WriteLine(path.Text + @"\run.bat");
+            Process.Start(server);
+        }
+
+        private void plugins_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo x = new ProcessStartInfo
+            {
+                FileName = @"C:\Windows\explorer.exe",
+                Arguments = path.Text + @"\plugins"
+            };
+            Process.Start(x);
+        }
+
+        private void properties_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo properties = new ProcessStartInfo
+            {
+                FileName = @"C:\Windows\notepad.exe",
+                Arguments = path.Text + @"\server.properties"
+            };
+            Process.Start(properties);
+        }
+
+        private void RunArgument_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo x = new ProcessStartInfo
+            {
+                FileName = "notepad.exe",
+                Arguments = path.Text + @"\run.bat"
+            };
+            Process.Start(x);
+        }
+        private void Scripts_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                File.ReadAllText(path.Text + @"\plugins\Skript.jar");
+                ProcessStartInfo x = new ProcessStartInfo
+                {
+                    FileName = @"C:\Windows\explorer.exe",
+                    Arguments = path.Text + @"\plugins\Skript\scripts"
+                };
+                Process.Start(x);
+            }
+            catch
+            {
+                DialogResult result = MessageBox.Show("Do you want to download it?", "Skript plugin is not found!", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    WebClient webClient = new WebClient();
+                    webClient.DownloadFile("https://github.com/SkriptLang/Skript/releases/download/2.5.2/Skript.jar", path.Text + @"\plugins\Skript.jar");
+                }
+            }
+        }
+    }
+}
